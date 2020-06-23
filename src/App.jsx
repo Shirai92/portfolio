61b462d738abd57307c9d188e72607937ac730a8
Shirai <shirai92@gmail.com>
2020-06-23 22:10:53 +0200

    delete footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,31 @@ import "./App.css";
 import Routes from "./routes";
 import Nav from "./components/nav";
 import { BrowserRouter as Router } from "react-router-dom";
-import Foter from "./components/footer";
 import { Layout } from "antd";
 
 function App() {
   const { Content } = Layout;
   return (
     <>
-      <Layout style={{ 
-        minHeight: "100vh",
-        backgroundColor: "white" }}>
+      <Layout
+        style={{
+          minHeight: "100vh",
+          backgroundColor: "white",
+        }}
+      >
         <Router>
           <Nav />
 
-          <Content style={{ 
-            marginLeft: "250px", 
-            marginRight: "250px", 
-            backgroundColor: "#F0F2F5", 
-             }}>
+          <Content
+            style={{
+              marginLeft: "250px",
+              marginRight: "250px",
+              backgroundColor: "#F0F2F5",
+            }}
+          >
             <Routes></Routes>
           </Content>
         </Router>
-        {/* <Foter  /> */}
       </Layout>
     </>
   );
