bb90fc538a1b665fa4d4237422a47d0d76f17f26
Shirai <shirai92@gmail.com>
2020-06-24 20:09:03 +0200

    start to do project

--- src/pages/projects/project3/project3.jsx
+++ src/pages/projects/project3/project3.jsx
@@ -1,9 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+import AddTask from "./addTask";
+import TaskList from "./taskList";
 
-const Project3 = () =>{
+// const x = {
+//     id: 0,
+//     text: "Lorem0",
+//     date: "2020-10-09",
+//     important: true,
+//     active: false,
+//     finishDate: null,
+//   }
 
-    return(
-        <div>Project3</div>
-    )
-}
+//   const y = {
+//       ...x,
+//       active: true
+//   }
+
+const Project3 = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 0,
+      text: "Lorem0",
+      date: "2020-10-09",
+      important: true,
+      active: false,
+      finishDate: null,
+    },
+    {
+      id: 1,
+      text: "Lorem1",
+      date: "2021-10-09",
+      important: true,
+      active: false,
+      finishDate: null,
+    },
+    {
+      id: 2,
+      text: "Lorem2",
+      date: "2022-10-09",
+      important: false,
+      active: true,
+      finishDate: null,
+    },
+    {
+      id: 3,
+      text: "Lorem3",
+      date: "2023-10-09",
+      important: true,
+      active: true,
+      finishDate: null,
+    },
+  ]);
+
+  const deleteTask = (id) => {
+    console.log("delete id" + id);
+    const listWihoutSelectedElement = tasks.filter((task) => task.id !== id);
+
+    setTasks(listWihoutSelectedElement);
+  };
+  const changeTaskStatus = (id) => {
+    console.log("change id" + id);
+
+    const arr = tasks.map((task) => {
+      if (task.id === id) {
+        return {
+          ...task,
+          active: !task.active,
+        };
+      }
+      return task;
+    });
+    setTasks(arr);
+    console.log(arr);
+  };
+  return (
+    <>
+      <div>TODO APP</div>
+      <AddTask></AddTask>
+      <TaskList tasks={tasks} delete={deleteTask} change={changeTaskStatus} />
+    </>
+  );
+};
 export default Project3;
